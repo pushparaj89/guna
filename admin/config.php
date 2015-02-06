791f1e024056d94bb706fb0bb9beff60cde71be2
Pushparaj <pushparaj.g.89@gmail.com>
2015-02-06 19:49:42 +0530

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.local/admin/');
+define('HTTP_CATALOG', 'http://opencart.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.local/admin/');
+define('HTTPS_CATALOG', 'http://opencart.local/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\projects\opencart\upload/admin/');
+define('DIR_SYSTEM', 'E:\projects\opencart\upload/system/');
+define('DIR_LANGUAGE', 'E:\projects\opencart\upload/admin/language/');
+define('DIR_TEMPLATE', 'E:\projects\opencart\upload/admin/view/template/');
+define('DIR_CONFIG', 'E:\projects\opencart\upload/system/config/');
+define('DIR_IMAGE', 'E:\projects\opencart\upload/image/');
+define('DIR_CACHE', 'E:\projects\opencart\upload/system/cache/');
+define('DIR_DOWNLOAD', 'E:\projects\opencart\upload/system/download/');
+define('DIR_UPLOAD', 'E:\projects\opencart\upload/system/upload/');
+define('DIR_LOGS', 'E:\projects\opencart\upload/system/logs/');
+define('DIR_MODIFICATION', 'E:\projects\opencart\upload/system/modification/');
+define('DIR_CATALOG', 'E:\projects\opencart\upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.local/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\projects\opencart\upload/catalog/');
+define('DIR_SYSTEM', 'E:\projects\opencart\upload/system/');
+define('DIR_LANGUAGE', 'E:\projects\opencart\upload/catalog/language/');
+define('DIR_TEMPLATE', 'E:\projects\opencart\upload/catalog/view/theme/');
+define('DIR_CONFIG', 'E:\projects\opencart\upload/system/config/');
+define('DIR_IMAGE', 'E:\projects\opencart\upload/image/');
+define('DIR_CACHE', 'E:\projects\opencart\upload/system/cache/');
+define('DIR_DOWNLOAD', 'E:\projects\opencart\upload/system/download/');
+define('DIR_UPLOAD', 'E:\projects\opencart\upload/system/upload/');
+define('DIR_MODIFICATION', 'E:\projects\opencart\upload/system/modification/');
+define('DIR_LOGS', 'E:\projects\opencart\upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
